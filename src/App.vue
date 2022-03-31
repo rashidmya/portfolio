cac91d4419a84b9e327416b529e2c7d75fe6beb9
buga-sys <itsfreak69@gmail.com>
2022-03-31 10:56:25 +0400

    minimal

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import TheGreeting from '@/components/TheGreeting.vue'
+import TheGreeting from "@/components/TheGreeting.vue";
 </script>
 
 <template>
@@ -8,32 +7,78 @@ import TheGreeting from '@/components/TheGreeting.vue'
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
-      <TheGreeting/>
+      <TheGreeting />
 
-      <nav>
-        <RouterLink to="/" >return { graphicDesign }</RouterLink>
-        <RouterLink to="/services">return { services }</RouterLink>
-        <RouterLink to="/projects">return { projects }</RouterLink>
-      </nav>
+      <div class="nav">
+        <div class="nav-wrapper">
+          <a href="https://www.linkedin.com/in/rashidalawadhi/" class="li icon"
+            ><fa icon="linkedin" type="fab"></fa
+          ></a>
+          <a href="https://github.com/rashidmya" class="gh icon"
+            ><fa icon="github" type="fab"></fa
+          ></a>
+          <a href="mailto:rashid.myaa@gmail.com" class="mail icon"
+            ><fa icon="envelope" type="fas"></fa
+          ></a>
+        </div>
+      </div>
     </div>
   </header>
-
-
-  <router-view v-slot="{ Component }">
-  <transition name="scale" mode="out-in">
-    <component :is="Component" />
-  </transition>
-</router-view>
 </template>
 
 <style>
-@import '@/assets/base.css';
+@import "@/assets/base.css";
+.nav {
+  width: 100%;
+}
+
+.icon {
+  color: white;
+  width: 50px;
+  height: 50px;
+  margin: 1em;
+  background-color: blue;
+  padding: 8px;
+  border-radius: 10px;
+}
+
+.mail {
+  background: rgb(131, 171, 255);
+  background: linear-gradient(
+    0deg,
+    rgba(131, 171, 255, 1) 0%,
+    rgba(33, 105, 255, 1) 100%
+  );
+}
+
+.mail:hover {
+  background: rgb(104, 151, 255);
+  background: linear-gradient(0deg, rgb(102, 151, 255) 0%, rgb(0, 81, 255) 100%);
+}
+.gh {
+  background: rgb(60, 60, 60);
+  background: linear-gradient(0deg, rgba(60, 60, 60, 1) 0%, rgba(71, 71, 71, 1) 100%);
+}
+
+.gh:hover {
+  background: rgb(49, 49, 49);
+  background: linear-gradient(0deg, rgb(49, 49, 49) 0%, rgb(56, 56, 56) 100%);
+}
+
+.li {
+  background: rgb(31, 69, 180);
+  background: linear-gradient(0deg, rgba(31, 69, 180, 1) 0%, rgba(31, 69, 180, 1) 100%);
+}
+
+.li:hover {
+  background: rgb(0, 45, 179);
+  background: linear-gradient(0deg, rgb(10, 51, 177) 0%, rgb(15, 57, 180) 100%);
+}
 
 .scale-enter-active,
 .scale-leave-active {
   transition: all 0.5s ease;
 }
-
 
 .scale-enter-from,
 .scale-leave-to {
@@ -72,29 +117,13 @@ a,
   }
 }
 
-nav {
+.nav-wrapper {
+  display: flex;
   width: 100%;
   font-size: 1em;
   text-align: center;
   margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
+  justify-content: center;
 }
 
 @media (min-width: 1024px) {
@@ -112,7 +141,7 @@ nav a:first-of-type {
   header {
     display: flex;
     place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    padding-right: calc(var(--section-gap) / 10);
   }
 
   header .wrapper {
@@ -133,5 +162,25 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
+
+  .nav-wrapper {
+    text-align: left;
+    margin-left: 1rem;
+    font-size: 1rem;
+    padding: 1rem 0;
+    margin-top: 1rem;
+    justify-content: left;
+  }
+
+  .icon {
+  color: white;
+  width: 50px;
+  height: 50px;
+  margin: 1em;
+  background-color: blue;
+  padding: 8px;
+  border-radius: 10px;
+  margin: 0 1em;
+}
 }
 </style>
